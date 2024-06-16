8d961959293b9a50575c02409ceee44a5442153d
JhonatanCarlosChavez <i202122464@cibertec.edu.pe>
2024-06-16 11:08:38 -0500

    Ejercicio Completado

---
+++ src/main/java/com/cibertec/ciber/service/ProductoService.java
@@ -0,0 +1,10 @@
+package com.cibertec.ciber.service;
+
+import java.util.List;
+
+import com.cibertec.ciber.entity.Producto;
+
+public interface ProductoService {
+    
+     public abstract List<Producto> listaTodos();
+}
